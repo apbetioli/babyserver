a585b4f44773a0e13e18059faf92baaaf576500a
Alexandre Betioli <apbetioli@gmail.com>
2019-10-26 23:11:35 -0300

    Reorganizing files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
     res.send("Hello baby");
 });
 
-router.post('/api/contact', (req, res, next) => {
+router.post('/api/contact', (req, res) => {
     console.log(req.body);
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ status: `Hello from Backend ${req.body.name}!` }));
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan')
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require('./routes.js'));
+app.use(morgan('dev'));
 
 app.listen(process.env.PORT || 3001, () =>
   console.log('Express server is running on localhost:3001')
