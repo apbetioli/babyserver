55bffbedc4a3835786dea65903aac12724fcc25d
Alexandre Betioli <apbetioli@gmail.com>
2019-10-28 14:24:52 -0300

    Send contact mail

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,43 +1,28 @@
-const path = require('path');
 const nodemailer = require('nodemailer');
 const config = require('../config/mail.json');
-const hbs = require('nodemailer-express-handlebars');
+const mg = require('nodemailer-mailgun-transport');
 
-var transport = nodemailer.createTransport(config);
+const auth = {
+    auth: {
+        api_key: process.env.MAILGUN_PRIVATE_KEY,
+        domain: process.env.MAILGUN_DOMAIN
+    }
+}
 
-var options = {
-    viewEngine: {
-        extname: '.html', // handlebars extension
-        layoutsDir: 'src/resources/mail/', // location of handlebars templates
-        defaultLayout: 'template', // name of main template
-        partialsDir: 'src/resources/mail/', // location of your subtemplates aka. header, footer etc
-    },
-    viewPath: 'src/resources/mail/',
-    extName: '.html'
-};
+const transport = nodemailer.createTransport(mg(auth));
 
-transport.use('compile', hbs(options));
-
-var send = async contact => {
+const send = async contact => {
     const mail = {
         to: config.to,
         from: "'" + contact.name + "' " + contact.email,
         subject: config.subject,
-        template: 'contact',
-        context: {
-            name: contact.name,
-            email: contact.email,
-            message: contact.message
-        }
+        text: contact.message,
+        html: contact.message
     };
-    console.log(mail);
+
     const info = await transport.sendMail(mail);
 
-    console.log('Message sent: %s', info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log('Contact sent: %s', info.messageId);
 };
 
 module.exports = { send };
