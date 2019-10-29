d4eee905c63db3afb9420e2c1d06e574bcf68558
Alexandre Betioli <apbetioli@gmail.com>
2019-10-29 14:20:40 -0300

    Enable cors preflight

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const morgan = require('morgan')
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  "preflightContinue": true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require('./app/controllers/pingController'));
